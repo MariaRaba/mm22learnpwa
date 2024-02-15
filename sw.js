ea88274aeb217906dce5632cb6b9262777aad4d5
MariaRaba <Maria.Raba@tptlive.ee>
2024-02-15 21:08:52 +0200

    Plsworks

---
+++ sw.js
@@ -0,0 +1,25 @@
+let cacheName= "hello-pwa";
+let filesToCache = [
+    '/',
+    '/index.html',
+    '/style.css',
+    '/main.js',
+];
+
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache){
+            return cache.addAll(filesToCache);
+        })
+    );
+});
+
+self.addEventListener('fetch', function(e){
+    e.respondWith(
+        cache.match(e.request).then(function (response){
+            return response || fetch(e.request);
+        })
+    );
+});
+
+//Offlines leht ei tööta, + "uncaught referenceerror: cache is not defined" (2:02:20). Future me, how are you doing?
